b7b449f3164b278450f4305b70a43b04bb8f0c66
VishnukantMule <20104065@apsit.edu.in>
2022-10-04 15:46:17 +0530

    Addin Header Poster

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import '../../App.css';
+import img from '../images/havana.png'
+import '../PagesCss/Home.css'
 
 export default function Home() {
   return (
     <>
-      <h1 className='home'>Pravah Studio</h1>
+    <div className='cardp'>
+          <header className="polaroid"><video className='havana' id="video" controls preload="none" poster={img}>
+  <source id="mp4" src='https://res.cloudinary.com/dsp4p9uyy/video/upload/v1664878460/EnglishSongs/Camila_Cabello_-__Havana___live_at_Capital_s_Summertime_Ball_2018_720P_HD_1_sd2xfn.mp4' type="video/mp4" />
+
+</video>
+      
+
+      </header>
+    </div>
+   
+
+
+
+    
     </>
   );
 }
